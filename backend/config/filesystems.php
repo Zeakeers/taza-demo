e3e5cb13a1a87e4b0f1101680edf4d42da1163c3
DIMAS ADI PUTRA ARIFIN <dimasadiputraarifin@gmail.com>
2026-04-29 10:39:33 +0700

    feat: add new hero section graphics and update home page layout

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -47,6 +47,15 @@ return [
             'report' => false,
         ],
 
+        'nextjs_public' => [
+            'driver' => 'local',
+            'root' => base_path('../public/uploads'),
+            'url' => '/uploads',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
